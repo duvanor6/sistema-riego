3eed506d1736006740b2951b2f7376ef3820d6bd
Duban Ortega Pacheco <duortega@soaint.com>
2023-11-23 08:53:07 -0500

    septimo commit

--- auth.php
+++ auth.php
@@ -17,10 +17,10 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
     //delvolvemos una respuesta
     header('Content-Type: application/json');
-    header("Access-Control-Allow-Origin: http://localhost:8080");
+    header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: POST"); // Ajusta los métodos permitidos según tus necesidades
     header("Access-Control-Allow-Headers: Content-Type");
-    
+
     if(isset($datosArray["result"]["error_id"])){
         $responseCode = $datosArray["result"]["error_id"];
         http_response_code($responseCode);
